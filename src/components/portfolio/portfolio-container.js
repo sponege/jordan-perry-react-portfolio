60aa7bb485e94cbc32800c8e08ef7d90ada566ac
Jordan Perry <apples@jappl.es>
2021-06-29 18:41:48 -0600

    module one done

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+import PortfolioItem from "./portfolio-item"
+
+export default class PortfolioContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: [
+        { title: "Apple", category: "Fruit" },
+        { title: "Banana", category: "Fruit" },
+        { title: "Corn", category: "Veggie" }
+      ]
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category == filter
+      })
+    })
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem title={item.title} />;
+    })
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+      <h2>{this.state.pageTitle}</h2>
+
+      <button onClick={() => this.handleFilter("Fruit")}>Fruit</button>
+      <button onClick={() => this.handleFilter("Veggie")}>Veggie</button>
+
+      {this.portfolioItems()}
+      </div>
+    )
+  }
+}
